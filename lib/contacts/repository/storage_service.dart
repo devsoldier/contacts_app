50afe3e69ffadaa46b02d21f10f265703e3c26cc
devsoldier <yazeedwong@gmail.com>
2023-08-23 06:40:44 +0800

    storage service: added checkhivebox

--- lib/contacts/repository/storage_service.dart
+++ lib/contacts/repository/storage_service.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
 
+import 'package:collection/collection.dart';
 import 'package:contacts_app/contacts/repository/data_classes/contacts_details.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
@@ -12,11 +14,13 @@ const kContacts = 'kContacts';
 
 class StorageService {
   late final Box hiveBox;
+  bool hiveExist = false;
   List<ContactsDetails?> contactsList = [];
 
   Future<void> hiveSetup() async {
     try {
       var appDir = await getApplicationDocumentsDirectory();
+      await checkHiveBoxExist(appDir);
       var hiveDb = Directory('${appDir.path}/$kBoxDirectoryPath');
       Hive.init(hiveDb.path);
       hiveBox = await Hive.openBox(kContactsBox);
@@ -26,13 +30,25 @@ class StorageService {
     }
   }
 
-  storeContacts(List<ContactsDetails?> value) async {
+  Future<void> checkHiveBoxExist(Directory appDir) async {
+    final files = appDir.listSync(recursive: false, followLinks: false);
+    final hive = files.firstWhereOrNull((e) => e.path
+        .toLowerCase()
+        .contains('/app_flutter/$kBoxDirectoryPath'.toLowerCase()));
+
+    if (hive != null) {
+      hiveExist = true;
+    }
+    log('hive exist: $hiveExist');
+  }
+
+  Future<void> storeContacts(List<ContactsDetails?> value) async {
     final json = jsonEncode(value);
     await hiveBox.put(kContacts, json);
   }
 
-  retrieveContacts() async {
-    final box = hiveBox.get(kContacts);
+  Future<void> retrieveContacts() async {
+    final box = await hiveBox.get(kContacts);
     if (box != null) {
       final decoded = jsonDecode(box);
       log('${decoded.runtimeType}');
